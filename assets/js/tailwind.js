42da0569035bf5e0ab27177d631d68decc4058b2
VrBnDev <rafaelavborgess@gmail.com>
2025-07-24 22:58:40 -0300

    Atualizando o documento do minicurso.

---
+++ assets/js/tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                primary: '#00525d',
+                secondary: '#ffc107',
+                accent: '#4fc3f7',
+                light: '#f5f5f5',
+                dark: '#333333'
+            },
+            fontFamily: {
+                sans: ['Poppins', 'sans-serif']
+            }
+        }
+    }
+}
